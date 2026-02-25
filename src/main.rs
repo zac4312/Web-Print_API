86613f0e00ab46581000e974f1c5a50ec289d71c
zac4312 <sushi.ninja315@gmail.com>
2026-02-25 21:34:20 +0000

    implemented query all in main | todo: move to service

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod models;
-
-fn main() {
+use sqlx::{PgPool, postgres::PgRow};
+use models::md_user::User;
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
     println!("Hello, world!"); 
+    let test = PgPool::connect("postgresql://zacm:1234@localhost:5432/webprintingapi_db").await?;
+    let res = get(&test).await?; 
+    println!("{:?}", res);
+    Ok(()) 
 }
+
+async fn get(con: &PgPool) -> Result<Vec<PgRow>, sqlx::Error> {
+     let act = sqlx::query(r#" SELECT * FROM users "#)
+         .fetch_all(con)
+         .await?;
+        
+     Ok(act)
+}
+
