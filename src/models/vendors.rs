f872a59945cf2bbf3186af4cdfac660449972523
zac4312 <sushi.ninja315@gmail.com>
2026-03-07 16:33:26 +0000

    vendor issue: mapping to database for custom enum vacancy not working FIX

---
+++ src/models/vendors.rs
@@ -0,0 +1,37 @@
+use sqlx::{postgres::types::PgPoint, prelude::FromRow};
+use bigdecimal::BigDecimal;
+use crate::utils;
+
+#[derive(sqlx::Type, Debug)]
+#[sqlx(type_name = "vacancy", rename_all = "snake_case")]
+pub enum Vacancy{
+    Available,
+    SBusy,
+    Busy,
+    Closed,
+}
+
+#[derive(FromRow, Debug)]
+pub struct Vendor {
+    pub pub_id: String,
+    pub name: String,
+    pub pw_hash: String,
+    pub email: String,
+    pub bw_rate: BigDecimal,
+    pub clrd_rate: BigDecimal,
+    pub location: PgPoint,
+    pub availability: Vacancy,
+}
+
+impl Vendor {
+    pub fn new(name: String, 
+                    pw_hash: String,
+                    email: String,
+                    bw_rate: BigDecimal,
+                    clrd_rate: BigDecimal,
+                    location: PgPoint,
+                    availability: Vacancy) -> Self { 
+
+        Self {  pub_id: utils::generate_id(8) , name, pw_hash, email, bw_rate, clrd_rate, location, availability} }
+}
+
---
+++ src/service/vendor.rs
@@ -0,0 +1,32 @@
+use sqlx::{Pool, Postgres, postgres::PgRow};
+
+use crate::models::{vendors::Vendor};
+
+pub async fn create_vendor(con: &Pool<Postgres>, vendor: Vendor) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "
+        INSERT INTO vendors (name, pw_hash, email, bw_rate, clrd_rate, location, availability) 
+         VALUES ($1, $2, $3, $4, $5, $6, $7::vacancy);
+        ",
+        vendor.name, vendor.pw_hash, vendor.email, vendor.bw_rate, vendor.clrd_rate, vendor.location, vendor.availability,
+    )       
+    .execute(con)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn get_vendor(con: &Pool<Postgres>) -> Result<Vec<PgRow>, sqlx::Error> {
+     let act = sqlx::query(
+         
+         r#" 
+         SELECT * FROM vendors; 
+         "#)
+
+    .fetch_all(con)
+    .await?;
+        
+     Ok(act) 
+}
+
+
