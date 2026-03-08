d1bf00182176b628e694e48c07d4ee35b8497169
zac4312 <sushi.ninja315@gmail.com>
2026-03-08 13:04:01 +0000

    added: reference docs

---
+++ docs/DBschema_ref.sql
@@ -0,0 +1,43 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+create type vacancy as enum ('available', 's_busy', 'busy', 'closed');
+create type size as enum ('a4', 'a5', 'a3', 'letter', 'legal', 'tabloid');
+
+CREATE TABLE users(    
+    user_id uuid PRIMARY KEY default gen_random_uuid(),
+    name varchar(16) not null,
+    pw_hash varchar(255) not null,
+     pub_id varchar(20) not null unique
+);
+
+CREATE TABLE vendors ( --now vedors-- 
+    partner_id uuid PRIMARY KEY  default gen_random_uuid(),
+    name varchar(25) not null,
+    pw_hash varchar(255)not null,
+    bw_rate decimal(10,2) not null,
+    clrd_rate decimal(10,2) not null,
+    location point not null,
+    availability vacancy not null,
+    pub_id varchar(20) not null unique
+);
+
+CREATE TABLE orders (
+    order_id uuid PRIMARY KEY  default gen_random_uuid(),
+    copies SmallInt not null,
+    print_size size not null,
+    color boolean not null,
+    dt_stamp TimeStamptz default now(), 
+    pub_id varchar(20) not null unique,
+    
+    file_id uuid not null references files(file_id),
+    client_id uuid not null references users(user_id),
+    shop_id  uuid not null references partners(partner_id)
+);
+
+CREATE TABLE files (
+    file_id uuid PRIMARY KEY default gen_random_uuid(),
+    file_path text not null,
+    file_size BigInt not null,
+    mime_type varchar(100) not null,
+    deleted_on TimeStamptz
+)
