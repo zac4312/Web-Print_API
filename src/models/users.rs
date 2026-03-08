14c6b7a841193319f18b561b892a14ed4087dd34
zac4312 <sushi.ninja315@gmail.com>
2026-03-08 12:13:39 +0000

    added: usr model generates public id in fn() new

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,18 +1,23 @@
-use uuid::Uuid;
+use crate::{utils};
+use sqlx::{prelude::FromRow};
 
+#[derive(FromRow, Debug)]
 pub struct User {
-    pub user_id: Uuid,
+    pub pub_id: String,
     pub name: String,
     pub pw_hash: String,
+    pub email: String,
 }
 
 impl User {
-    pub fn new(name: String, pw_hash: String) -> Self {
+    pub fn new(name: String, pw_hash: String, email: String) -> Self {
        Self { 
-           user_id: Uuid::new_v4(),
+           pub_id: "usr".to_owned() +  &utils::generate_id(8),
            name,
+           email,
            pw_hash
-       } 
-    }     
+       }
+    } 
+     
 }
 
