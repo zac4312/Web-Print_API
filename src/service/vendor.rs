1600d9186f2250023ef7aa37c5e793a66b19de20
zac4312 <sushi.ninja315@gmail.com>
2026-03-08 12:27:58 +0000

    updated: usr service to integrate with db table

--- src/service/vendor.rs
+++ src/service/vendor.rs
@@ -1,14 +1,14 @@
 use sqlx::{Pool, Postgres, postgres::PgRow};
 
-use crate::models::{vendors::Vendor};
+use crate::models::vendors::{Vacancy, Vendor};
 
 pub async fn create_vendor(con: &Pool<Postgres>, vendor: Vendor) -> Result<(), sqlx::Error> {
-    sqlx::query!(
+   sqlx::query!(
         "
-        INSERT INTO vendors (name, pw_hash, email, bw_rate, clrd_rate, location, availability) 
-         VALUES ($1, $2, $3, $4, $5, $6, $7::vacancy);
+        INSERT INTO vendors (name, pw_hash, email, bw_rate, clrd_rate, location, availability, pub_id) 
+         VALUES ($1, $2, $3, $4, $5, $6, $7::vacancy, $8);
         ",
-        vendor.name, vendor.pw_hash, vendor.email, vendor.bw_rate, vendor.clrd_rate, vendor.location, vendor.availability,
+        vendor.name, vendor.pw_hash, vendor.email, vendor.bw_rate, vendor.clrd_rate, vendor.location, vendor.availability as Vacancy, vendor.pub_id 
     )       
     .execute(con)
     .await?;
