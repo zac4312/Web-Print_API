9d41573a63c5bb165b5de9efffebbd728134b8ff
zac4312 <sushi.ninja315@gmail.com>
2026-03-14 15:28:14 +0000

    migration: refractor order table

--- migrations/010_reformat_order.sql
+++ migrations/010_reformat_order.sql
@@ -1,5 +1,13 @@
+ALTER TABLE vendors
+     drop column partner_id cascade,
+     add column vendor_id uuid PRIMARY KEY default gen_random_uuid();
+
 ALTER TABLE orders 
     drop column dt_stamp,
-    drop ;
+    drop column client_id,
+    drop column shop_id,
+
+    add column for_vendor uuid not null references vendors(vendor_id),
+    add column for_user uuid not null references users(user_id);
 
 
