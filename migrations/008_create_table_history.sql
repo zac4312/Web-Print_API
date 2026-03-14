cf63333ee010069eebd69e7e0d1e23db94deb4db
zac4312 <sushi.ninja315@gmail.com>
2026-03-14 15:25:38 +0000

    migrations: add history table

--- migrations/008_create_table_history.sql
+++ migrations/008_create_table_history.sql
@@ -3,8 +3,10 @@ create type state as enum ('rejected', 'paid', 'claimed', 'completed');
 CREATE TABLE order_history (
     history_id uuid PRIMARY KEY default gen_random_uuid(),
     order_status state not null,
-    created_at TimeStampz default now(),
-    paid_at TimeStampz,
-    claimed_at TimeStampz,
-    completed_at TimeStampz
-)
+    created_at TimeStamptz default now(),
+    paid_at TimeStamptz,
+    claimed_at TimeStamptz,
+    completed_at TimeStamptz,
+
+    order_of uuid not null references orders(order_id)
+);
